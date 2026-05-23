96a976d5e803a2037c0c78b5732d043e33824f32
harungunay1334 <harungunay1334@gmail.com>
2026-05-23 13:45:11 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-// Devre dışı — eski PWA service worker kayıtlarını temizlemek için
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).then(() => self.clients.claim())
-  );
-});
